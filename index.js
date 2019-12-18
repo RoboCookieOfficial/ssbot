808e7490c9fa948771c0187e011cdc2c9f94e4ca
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2019-12-18 15:48:54 +0100

    init
    init

--- index.js
+++ index.js
@@ -31,6 +31,23 @@ client.on("guildDelete", guild => {
   client.user.setActivity(`/help`);
 });
 
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+
 client.on("message", async message => {
   if(message.author.bot) return;
   
