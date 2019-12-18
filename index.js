5ae6c417adeb609c0656855b50bd8ea73383f7aa
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2019-12-18 14:14:13 +0100

    init
    init

--- index.js
+++ index.js
@@ -399,6 +399,80 @@ client.on("message", async message => {
          let sChannel = message.guild.channels.find(c => c.name === "logs")
          sChannel.send(embed)
   }
+
+  if(command === 'giveaway') {
+    var item = "";
+    var time;
+    var winnerCount;
+ 
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, you can't use this command");
+ 
+ 
+    winnerCount = args[0];
+    time = args[1];
+    item = args.splice(2, args.length).join(' ');
+ 
+    message.delete();
+ 
+    var date = new Date().getTime();
+    var dateTime = new Date(date + (time * 1000));
+ 
+    var giveawayEmbed = new discord.RichEmbed()
+        .setTitle("🎉 **GIVEAWAY** 🎉")
+        .setFooter(`Ends at: ${dateTime}`)
+        .setDescription(item);
+ 
+    var embedSend = await message.channel.send(giveawayEmbed);
+    embedSend.react("🎉");
+ 
+    setTimeout(function () {
+ 
+        var random = 0;
+        var winners = [];
+        var inList = false;
+ 
+        var peopleReacted = embedSend.reactions.get("🎉").users.array();
+ 
+        for (var i = 0; i < peopleReacted.length; i++) {
+            if (peopleReacted[i].id == bot.user.id) {
+                peopleReacted.splice(i, 1);
+                continue;
+            }
+        }
+ 
+        if (peopleReacted.length == 0) {
+            return message.channel.send("Nobody reacted, no winners");
+        }
+ 
+        if (peopleReacted.length < winnerCount) {
+            return message.channel.send("To few people reacted, no winners");
+        }
+         for (var i = 0; i < winnerCount; i++) {
+ 
+            inList = false;
+ 
+            random = Math.floor(Math.random() * peopleReacted.length);
+ 
+            for (var y = 0; y < winners.length; y++) {
+                if (winners[y] == peopleReacted[random]) {
+                    i--;
+                    inList = true;
+                    break;
+                }
+            }
+ 
+            if (!inList) {
+                winners.push(peopleReacted[random]);
+            }
+ 
+        }
+ 
+        for (var i = 0; i < winners.length; i++) {
+            message.channel.send("Congrats " + winners[i] + `! You won: **${item}**.`);
+        }
+ 
+    }, 1000 * time);
+  }
 });
 
 client.login(process.env.TOKEN);
