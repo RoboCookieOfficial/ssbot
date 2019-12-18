aa0c660775fbb86524a7625a47988bc57360e055
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2019-12-18 14:20:23 +0100

    init
    init

--- index.js
+++ index.js
@@ -473,6 +473,27 @@ client.on("message", async message => {
  
     }, 1000 * time);
   }
+
+  if(command === 'report') {
+	message.delete()
+
+        let target = message.mentions.members.first() || message.guild.members.get(args[0])
+        if(!target) return message.channel.send("Please provide a valid user").then(m => m.delete(15000))
+
+
+        let reason = args.slice(1).join(" ")
+        if(!reason) return message.channel.send(`Please provide a reason for reporting **${target.user.tag}**`).then(m => m.delete(15000))
+
+
+        let sChannel = message.guild.channels.find(x => x.name === "tut-reports")
+
+
+        message.channel.send("Your report has been filed to the staff team. Thank you!").then(m => m.delete(15000))
+        sChannel.send(`**${message.author.tag}** has reported **${target.user.tag}** for **${reason}**.`).then(async msg => {
+            await msg.react("✅")
+            await msg.react("❌")
+        })
+  }
 });
 
 client.login(process.env.TOKEN);
