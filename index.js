21cdfd73d9faa4caf748bff702f590c6d7df7e6e
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2019-12-18 14:40:21 +0100

    init
    init

--- index.js
+++ index.js
@@ -494,27 +494,6 @@ client.on("message", async message => {
             await msg.react("❌")
         })
   }
-	
-  if(command === 'meme') {
-	let msg = await message.channel.send("Generating...")
-
-        fetch("https://apis.duncte123.me/meme")
-        .then(res => res.json()).then(body => {
-            if(!body || !body.data.image) return message.reply(" whoops. I broke, try again!")
-
-            let memeEmbed = new RichEmbed()
-            .setColor(cyan)
-            .setAuthor(`${bot.user.username} meme!`, message.guild.iconURL)
-            .setImage(body.data.image)
-            .setTimestamp()
-            .setFooter(bot.user.username.toUpperCase(), bot.user.displayAvatarURL)
-
-            if(body.data.title) {
-                embed.setTitle(body.data.title).setURL(body.data.url)
-            }
-                msg.edit(memeEmbed)
-        })	
-  }
 });
 
 client.login(process.env.TOKEN);
