a0f1199b97e49484091e165fbf13ef222249df6f
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2019-12-21 13:40:42 +0100

    init
    init

--- index.js
+++ index.js
@@ -479,6 +479,25 @@ client.on("message", async message => {
 
     msg.delete();
   }
+
+  if(command === '8ba') {
+    	const responses = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes – definitely",
+		"You may rely on it",
+		"As I see it",
+		"yes",
+		"Most Likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes"
+	];
+
+	const randomIndex = Math.floor(Math.random() * responses.length);
+	bot.reply(message, responses[randomIndex]);
+  }
 });
 
 client.login(process.env.TOKEN);
