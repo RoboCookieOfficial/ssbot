941aabd342f3196efb3c362acd473932c792085e
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2020-02-12 19:10:24 +0100

    init

--- index.js
+++ index.js
@@ -122,11 +122,11 @@ client.on("message", async message => {
   }
   
   if(command === "clear") {
-    if(!message.member.hasPermission("MANAGE_MESSAGES") || !message.guild.owner) return message.channel.send("You dont have permission to use this command.");
+    if(!message.member.hasPermission("MANAGE_MESSAGES") || !message.guild.owner) return message.channel.send("**You dont have permission to use this command**");
     const deleteCount = parseInt(args[0], 10);
     
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+      return message.reply("**Please provide a number between 2 and 100 for the number of messages to delete**");
     
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
