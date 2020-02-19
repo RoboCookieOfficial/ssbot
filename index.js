49fcb024dc379026125453ce9467819be08071ae
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2020-02-19 18:53:48 +0100

    init

--- index.js
+++ index.js
@@ -35,12 +35,10 @@ client.on("ready", () => {
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`/help`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`/help`);
 });
 
 client.on("message", async message => {
