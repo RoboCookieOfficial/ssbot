bf1d6df174f737c26d6187f0786db93e0a8f5bd6
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2020-02-19 19:13:43 +0100

    init

--- index.js
+++ index.js
@@ -528,6 +528,20 @@ client.on("message", async message => {
 
      message.channel.send(memeEmbed);
   }
+	
+  if(command === 'WR_tips') {
+     const subReddits = ["War_Robots_tips", "War_Robots_tricks", "War_Robots_knowledge", "War_Robots_pro_tips"];
+     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+     const img = await randomPuppy(random);
+     const memeEmbed = new Discord.RichEmbed()
+         .setColor(EmbedColor)
+         .setImage(img)
+         .setTitle(`From /r/${random}`)
+         .setURL(`https://reddit.com/r/${random}`);
+
+     message.channel.send(memeEmbed);
+  }
 });
 
 client.login(process.env.TOKEN);
