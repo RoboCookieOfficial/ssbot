2104b05235030c5ad24e6565769a5d054a2895e7
RoboCookieOfficial <44526969+RoboCookieOfficial@users.noreply.github.com>
2020-02-19 19:24:31 +0100

    init

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const config = require("./config.json");
 
 const Welcome = require("discord-welcome");
 const randomPuppy = require("random-puppy");
+const google = require("google");
+
 
 var EmbedColor = (
   "#00FFFF"
@@ -555,6 +557,47 @@ client.on("message", async message => {
          .setURL(`https://reddit.com/r/${random}`);
 
      message.channel.send(memeEmbed);
+  }
+	
+  if(command === 'search') {
+     if (!suffix) {
+        message.channel.send({
+            embed: {
+                color: 0xff2727,
+                description: `:warning: **${message.author.username}**, You didn't give me anything to search. {.google \`input\`}`,
+              ```
+                }
+            }
+        });
+    }
+    google.resultsPerPage = 5;
+    google(suffix, function (err, res) {
+        if (err) message.channel.send({
+            embed: {
+                color: 0xff2727,
+                description: `:warning: **${message.author.username}**, ${err}`,
+                footer: {
+                    text: 'API Lantancy is ' + `${Date.now() - message.createdTimestamp}` + ' ms',
+                }
+            }
+        });
+        for (var i = 0; i < res.links.length; ++i) {
+            var link = res.links[i];
+            if (!link.href) {
+                res.next;
+            } else {
+                let embed = new Discord.RichEmbed()
+                    .setColor(`#ffffff`)
+                    .setAuthor(`Result for "${suffix}"`, `https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`)
+                    .setDescription(`**Link**: [${link.title}](${link.href})\n**Description**:\n${link.description}`)
+                    .setTimestamp()
+                    .setFooter('API Lantency is ' + `${Date.now() - message.createdTimestamp}` + ' ms', message.author.displayAvatarURL);
+                return message.channel.send({
+                    embed: embed
+                });
+            } return message.react("👌");
+        }
+    });
   }
 });
 
